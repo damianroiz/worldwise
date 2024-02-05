24ff6d37341a29a5d131d3f1367d3029ee5fdbd4
Damian Roiz <damianx989@gmail.com>
2024-02-05 08:10:35 -0500

    starting files

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,11 @@
+import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
+
+export default function AppLayout() {
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+    </div>
+  );
+}
